786d371c3dce013fe44811e3286b5113910c4fbd
Sergio Cartuche <sdcartuchem7895@gmail.com>
2026-05-17 19:03:58 -0500

    feat: create incident controller with management, synchronization, and statistics endpoints

--- src/admin/incident/incident.controller.ts
+++ src/admin/incident/incident.controller.ts
@@ -143,10 +143,10 @@ export class IncidentController {
   @AuthWithKeycloak()
   @Get('find-all-fraction-sanction-total/:userId/:idDevice/:version')
   findAllFractionSanctionTotal(
-    @Query() filterDto: FilterDto,
     @Param('userId', ParseIntPipe) userId: number,
     @Param('idDevice', ParseUUIDPipe) idDevice: string,
-    @Param('version', ParseIntPipe) version: number
+    @Param('version', ParseIntPipe) version: number,
+    @Query() filterDto: IncidentFilterDto,
   ) {
     return this.incidentService.findAllFractionSanctionTotal(filterDto);
   }
