8b2381abe5c9d4ab8bd40eb8e64f04a3959fd10a
Sergio Cartuche <sdcartuchem7895@gmail.com>
2026-05-21 12:53:38 -0500

    feat: create IncidentFilterDto for incident filtering and validation

--- src/admin/incident/dto/incident-filterdto.dto.ts
+++ src/admin/incident/dto/incident-filterdto.dto.ts
@@ -2,6 +2,7 @@ import { Type } from 'class-transformer';
 import { IsBoolean, IsEnum, IsNumber, IsOptional, IsString, Min } from 'class-validator';
 import { FilterDto } from 'src/common/dto/filter.dto';
 import { IncidentStatus } from 'src/common/glob/type/type_incident';
+import { InternalStateIncident } from 'src/common/glob/type/type_internal_state_incident';
 
 export class IncidentFilterDto extends FilterDto {
   @IsOptional()
@@ -38,6 +39,11 @@ export class IncidentFilterDto extends FilterDto {
   @Type(() => Number)
   @IsEnum(IncidentStatus)
   statusIncident?: IncidentStatus;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsEnum(InternalStateIncident)
+  internalState?: InternalStateIncident;
 
   @IsOptional()
   @IsNumber()
