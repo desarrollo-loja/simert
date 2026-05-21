2e624b92aec90451c1c85850e64c63f513fbe25a
Sergio Cartuche <sdcartuchem7895@gmail.com>
2026-05-21 17:47:07 -0500

    feat: add controller endpoint to retrieve incident payments filtered by transaction IDs

--- src/admin/incident-payment/incident-payment.controller.ts
+++ src/admin/incident-payment/incident-payment.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Patch } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { AuthWithKeycloak } from 'src/auth/decorators';
+import { AuthWithKeycloak, GetUser } from 'src/auth/decorators';
+import { JwtPayload } from 'src/auth/interfaces';
 import { FilterDto } from 'src/common/dto/filter.dto';
 import { TypeRol } from 'src/common/glob/type/type_rol';
 
@@ -13,8 +14,11 @@ export class IncidentPaymentController {
 
   @ApiOperation({ summary: 'List incident payments filtered by transactionIds (id, transactionId, optionalData)' })
   @AuthWithKeycloak(TypeRol.ADMIN)
-  @Patch('find-all-by-transaction-id')
-  findAllByTransactionId(@Body() filterDto: FilterDto) {
+  @Patch('find-all-by-transaction-id/:userId/:idDevice/:version')
+  findAllByTransactionId(
+    @GetUser() user: JwtPayload,
+    @Body() filterDto: FilterDto
+  ) {
     return this.incidentPaymentService.findAllByTransactionId(filterDto);
   }
 }
