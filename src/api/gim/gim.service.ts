9746cca17a7e477354a3b6f1a5d12e0030ae6445
Sergio Cartuche <sdcartuchem7895@gmail.com>
2026-06-01 17:12:21 -0500

    feat: implement GimService to handle incident synchronization and obligation issuance in GIM API

--- src/api/gim/gim.service.ts
+++ src/api/gim/gim.service.ts
@@ -152,6 +152,7 @@ export class GimService {
         const dataUserGim = await this.getUserByIdentityCardGim(createGimDto.identityCard);
 
         if (dataUserGim.errorCode !== ErrorCode.NONE) {
+          console.log("dataUserGim createNewNaturalPersonGim", JSON.stringify(dataUserGim));
           //CREAR EL CLIETE EN EL GIM SI NO EXISTE VERIFICAR SIE S RUC O PERSONA FINAL 
           const createClientGimDto = new CreateClientGimDto();
           createClientGimDto.identityCard = createGimDto.identityCard;
